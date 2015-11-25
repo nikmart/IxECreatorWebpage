7c1a98c2c0f9cbc6addee4cfff51cb73a4f8367a
Nik Martelaro <nmartelaro@gmail.com>
2015-11-25 06:39:02 +0000

    Working base app with a button to create a new project that console.logs on the server.

--- public/client.js
+++ public/client.js
@@ -1,27 +1,6 @@
 var socket = io();
 
-// send out LedOn message over socket
-function ledON() {
-    socket.emit('ledON');
+// send a message to the server to create the project
+function create_project() {
+    socket.emit('createProject');
 }
-
-// send out ledOFF message over socket
-function ledOFF() {
-    socket.emit('ledOFF');
-}
-
-// read the data from the message that the server sent and change the
-// background of the webpage based on the data in the message
-socket.on('data', function(data) {
-    console.log('data:', data);
-    switch(data) {
-        case 'light':
-            document.body.style.backgroundColor = 'white';
-            console.log('white')
-            break;
-        case 'shade':
-            document.body.style.backgroundColor = 'black';
-            console.log('black');
-            break;
-    }
-});
--- server.js
+++ server.js
@@ -3,50 +3,24 @@ var http = require('http');
 var app = express();
 var server = http.Server(app);
 var io = require('socket.io')(server);
-var serialport = require('serialport');
 
 // use express to create the simple webapp
 app.use(express.static('public'));
-
-// check to make sure that the user calls the serial port for the arduino when
-// running the server
-if(!process.argv[2]) {
-    console.error('Usage: node '+process.argv[1]+' SERIAL_PORT');
-    process.exit(1);
-}
-
-// start the seriaal port connection and read on newlines
-var serial = new serialport.SerialPort(process.argv[2], {
-    parser: serialport.parsers.readline('\r\n')
-});
 
 // start the socket connection and say if someone connects
 io.on('connect', function(socket) {
     console.log('a user connected');
 
-    // if you get the 'ledON' msg, send an 'h' to the arduino
-    socket.on('ledON', function() {
-        console.log('ledON');
-        serial.write('h');
-    });
-
-    // if you get the 'ledOFF' msg, send an 'l' to the arduino
-    socket.on('ledOFF', function() {
-        console.log('ledOFF');
-        serial.write('l');
-    });
-
-    // if you get the 'disconnet' message, say the user disconnected
+    // if you get the 'disconnect' message, say the user disconnected
     socket.on('disconnect', function() {
         console.log('user disconnected');
     });
-});
 
-// read the serial data coming back from the arduino
-// and send it off to the client using a socket msg
-serial.on('data', function(data) {
-    console.log('data:', data);
-    io.emit('data', data);
+    // if you get the 'createProject' message, create a new IxE project
+    socket.on('createProject', function() {
+        console.log('create new project');
+    });
+
 });
 
 // start the server and say waht port it is on
