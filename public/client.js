335c7d86550cd8c9ebf07adc17abed8bf50890ba
Nik Martelaro <nmartelaro@gmail.com>
2015-11-25 07:51:23 +0000

    Page can take in prject name and pass it to the server.
    Need to get the shell script inside of this and then use the name as an argument.

--- public/client.js
+++ public/client.js
@@ -2,5 +2,6 @@ var socket = io();
 
 // send a message to the server to create the project
 function create_project() {
-    socket.emit('createProject');
+    var name = document.getElementById("projName").value;
+    socket.emit('createProject', name);
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http');
 var app = express();
 var server = http.Server(app);
 var io = require('socket.io')(server);
+var child_process = require('child_process');
 
 // use express to create the simple webapp
 app.use(express.static('public'));
@@ -17,8 +18,11 @@ io.on('connect', function(socket) {
     });
 
     // if you get the 'createProject' message, create a new IxE project
-    socket.on('createProject', function() {
-        console.log('create new project');
+    socket.on('createProject', function(name) {
+        console.log('create new project:', name);
+        //can't get out of the working directory of the project
+        //run the project creator script
+
     });
 
 });
